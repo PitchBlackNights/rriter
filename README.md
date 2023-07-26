fb531950200d42a5e4db4df21505db439938c7cd
PitchBlackNights <pitchblacknights@proton.me>
2023-07-26 18:37:28 -0500

    Added more to handler.rs, added other licenses

--- README.md
+++ README.md
@@ -38,3 +38,61 @@ In order of priority (Greatest to Least)
 - [ ] Spell Checker
 - [ ] Popup Toolbar  (CTRL+t)
 - [ ] Printing
+
+## Licenses
+
+This project is licensed under GNU AGPL (Affero General Public License) (see `LICENSE.md`)
+
+`/src/text_input/handler.rs` contains code from `/src/backend/crossterm.rs` in [tui-input](https://github.com/sayanarijit/tui-input/blob/710017a4c29bbffffe1b04548c2bf2756f427023/src/input.rs) (Licensed under the MIT License)
+
+```Text
+MIT License
+
+Copyright (c) 2021 Arijit Basu
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+```
+
+`/src/{app.rs, event.rs, handler.rs, lib.rs, main.rs, tui.rs, ui.rs}` contains code from `/src/{app.rs, event.rs, handler.rs, lib.rs, main.rs, tui.rs, ui.rs}` in [rust-tui-template](https://github.com/ratatui-org/rust-tui-template/tree/33bde3d0a53732147a6cda4be17db0ac1dc10d8b/src) (Licensed under MIT License)
+
+```Text
+Permission is hereby granted, free of charge, to any
+person obtaining a copy of this software and associated
+documentation files (the "Software"), to deal in the
+Software without restriction, including without
+limitation the rights to use, copy, modify, merge,
+publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software
+is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice
+shall be included in all copies or substantial portions
+of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF
+ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
+SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
+```
--- src/text_input/handler.rs
+++ src/text_input/handler.rs
@@ -1,7 +1,11 @@
-use crate::app::{App, AppResult};
+// [InputRequest, StateChanged, InputResponse, Input (struct), Input (impl)]
+// The code contained in those mentioned functions, structures, types, and implements,
+// are from [Crate tui-input](https://github.com/sayanarijit/tui-input/blob/710017a4c29bbffffe1b04548c2bf2756f427023/src/input.rs)
+// The code is licensed under the MIT License (see `tui-input_LICENSE`).
+
 use crossterm::event::{KeyCode, KeyModifiers, KeyEvent};
 
-enum InputRequest {
+pub enum InputRequest {
     SetCursor(usize),
     InsertChar(char),
     GoToPrevChar,
@@ -16,6 +20,293 @@ enum InputRequest {
     DeleteNextWord,
     DeleteSelection,
     DeleteLine,
+}
+
+/// States modified response
+pub struct StateChanged {
+    pub value: bool,
+    pub cursor: bool,
+}
+pub type InputResponse = Option<StateChanged>;
+
+/// The input buffer with cursor support.
+pub struct Input {
+    value: String,
+    cursor: usize,
+}
+
+impl Input {
+    /// Initialize a new instance with a given value
+    /// Cursor will be set to the given value's length.
+    pub fn new(value: String) -> Self {
+        let len = value.chars().count();
+        Self { value, cursor: len }
+    }
+
+    /// Set the value manually.
+    /// Cursor will be set to the given value's length.
+    pub fn set_value(mut self, value: String) -> Self {
+        self.cursor = value.chars().count();
+        self.value = value;
+        self
+    }
+
+    /// Set the cursor manually.
+    /// If the input is larger than the value length, it'll be auto adjusted.
+    pub fn set_cursor(mut self, cursor: usize) -> Self {
+        self.cursor = cursor.min(self.value.chars().count());
+        self
+    }
+
+    // Reset the cursor and value to default
+    pub fn reset(&mut self) {
+        self.cursor = Default::default();
+        self.value = Default::default();
+    }
+
+    /// Handle request and emit response.
+    pub fn handle(&mut self, req: InputRequest) -> InputResponse {
+        use InputRequest::*;
+        match req {
+            SetCursor(pos) => {
+                let pos = pos.min(self.value.chars().count());
+                if self.cursor == pos {
+                    None
+                } else {
+                    self.cursor = pos;
+                    Some(StateChanged {
+                        value: false,
+                        cursor: true,
+                    })
+                }
+            }
+            InsertChar(c) => {
+                if self.cursor == self.value.chars().count() {
+                    self.value.push(c);
+                } else {
+                    self.value = self
+                        .value
+                        .chars()
+                        .take(self.cursor)
+                        .chain(
+                            std::iter::once(c)
+                                .chain(self.value.chars().skip(self.cursor)),
+                        )
+                        .collect();
+                }
+                self.cursor += 1;
+                Some(StateChanged {
+                    value: true,
+                    cursor: true,
+                })
+            }
+
+            DeletePrevChar => {
+                if self.cursor == 0 {
+                    None
+                } else {
+                    self.cursor -= 1;
+                    self.value = self
+                        .value
+                        .chars()
+                        .enumerate()
+                        .filter(|(i, _)| i != &self.cursor)
+                        .map(|(_, c)| c)
+                        .collect();
+
+                    Some(StateChanged {
+                        value: true,
+                        cursor: true,
+                    })
+                }
+            }
+
+            DeleteNextChar => {
+                if self.cursor == self.value.chars().count() {
+                    None
+                } else {
+                    self.value = self
+                        .value
+                        .chars()
+                        .enumerate()
+                        .filter(|(i, _)| i != &self.cursor)
+                        .map(|(_, c)| c)
+                        .collect();
+                    Some(StateChanged {
+                        value: true,
+                        cursor: false,
+                    })
+                }
+            }
+
+            GoToPrevChar => {
+                if self.cursor == 0 {
+                    None
+                } else {
+                    self.cursor -= 1;
+                    Some(StateChanged {
+                        value: false,
+                        cursor: true,
+                    })
+                }
+            }
+
+            GoToPrevWord => {
+                if self.cursor == 0 {
+                    None
+                } else {
+                    self.cursor = self
+                        .value
+                        .chars()
+                        .rev()
+                        .skip(self.value.chars().count().max(self.cursor) - self.cursor)
+                        .skip_while(|c| !c.is_alphanumeric())
+                        .skip_while(|c| c.is_alphanumeric())
+                        .count();
+                    Some(StateChanged {
+                        value: false,
+                        cursor: true,
+                    })
+                }
+            }
+
+            GoToNextChar => {
+                if self.cursor == self.value.chars().count() {
+                    None
+                } else {
+                    self.cursor += 1;
+                    Some(StateChanged {
+                        value: false,
+                        cursor: true,
+                    })
+                }
+            }
+
+            GoToNextWord => {
+                if self.cursor == self.value.chars().count() {
+                    None
+                } else {
+                    self.cursor = self
+                        .value
+                        .chars()
+                        .enumerate()
+                        .skip(self.cursor)
+                        .skip_while(|(_, c)| c.is_alphanumeric())
+                        .find(|(_, c)| c.is_alphanumeric())
+                        .map(|(i, _)| i)
+                        .unwrap_or_else(|| self.value.chars().count());
+
+                    Some(StateChanged {
+                        value: false,
+                        cursor: true,
+                    })
+                }
+            }
+
+            DeleteLine => {
+                if self.value.is_empty() {
+                    None
+                } else {
+                    let cursor = self.cursor;
+                    self.value = "".into();
+                    self.cursor = 0;
+                    Some(StateChanged {
+                        value: true,
+                        cursor: self.cursor == cursor,
+                    })
+                }
+            }
+
+            DeletePrevWord => {
+                if self.cursor == 0 {
+                    None
+                } else {
+                    let remaining = self.value.chars().skip(self.cursor);
+                    let rev = self
+                        .value
+                        .chars()
+                        .rev()
+                        .skip(self.value.chars().count().max(self.cursor) - self.cursor)
+                        .skip_while(|c| !c.is_alphanumeric())
+                        .skip_while(|c| c.is_alphanumeric())
+                        .collect::<Vec<char>>();
+                    let rev_len = rev.len();
+                    self.value = rev.into_iter().rev().chain(remaining).collect();
+                    self.cursor = rev_len;
+                    Some(StateChanged {
+                        value: true,
+                        cursor: true,
+                    })
+                }
+            }
+
+            DeleteNextWord => {
+                if self.cursor == self.value.chars().count() {
+                    None
+                } else {
+                    self.value = self
+                        .value
+                        .chars()
+                        .take(self.cursor)
+                        .chain(
+                            self.value
+                                .chars()
+                                .skip(self.cursor)
+                                .skip_while(|c| c.is_alphanumeric())
+                                .skip_while(|c| !c.is_alphanumeric()),
+                        )
+                        .collect();
+
+                    Some(StateChanged {
+                        value: true,
+                        cursor: false,
+                    })
+                }
+            }
+
+            GoToLineStart => {
+                if self.cursor == 0 {
+                    None
+                } else {
+                    self.cursor = 0;
+                    Some(StateChanged {
+                        value: false,
+                        cursor: true,
+                    })
+                }
+            }
+
+            GoToLineEnd => {
+                let count = self.value.chars().count();
+                if self.cursor == count {
+                    None
+                } else {
+                    self.cursor = count;
+                    Some(StateChanged {
+                        value: false,
+                        cursor: true,
+                    })
+                }
+            }
+
+            DeleteSelection => {
+                Some(StateChanged {
+                    value: false,
+                    cursor: false,
+                })
+            }
+        }
+    }
+
+    /// Get a reference to the current value.
+    pub fn value(&self) -> &str {
+        self.value.as_str()
+    }
+
+    /// Get the current cursor placement.
+    pub fn cursor(&self) -> usize {
+        self.cursor
+    }
 }
 
 pub fn handle_key(_key_event: KeyEvent) {
